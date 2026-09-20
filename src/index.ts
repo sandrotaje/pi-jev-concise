fb8797dfbcdd67f82d188c2ff19a16496d0a1f5d
Sandro Tajè <sandro.taje@gmail.com>
2026-09-20 15:01:45 +0200

    feat: export multi-signal gate API

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 export {
   DEFAULT_THRESHOLD,
+  DEFAULT_THRESHOLDS,
   DEFAULT_TIMEOUT_MS,
   evaluateConcision,
   formatScore,
   normalizeThreshold,
+  normalizeThresholds,
   revisionFeedback,
+  signalLabel,
+  thresholdHits,
   validProbability,
 } from "./concise.js";
 export type {
   ConcisionFailure,
   ConcisionPass,
   ConcisionScores,
+  ConcisionSignal,
+  ConcisionThresholds,
   ConcisionVerdict,
   EvaluateConcisionOptions,
+  ThresholdHit,
 } from "./concise.js";
-export { latestRoleMessage, latestRoleText, loadConfig } from "./extension.js";
+export { interventionLog, latestRoleMessage, latestRoleText, loadConfig, passLog } from "./extension.js";
